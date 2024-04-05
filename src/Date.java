3085d95e774e47493d49a45f6c51b312c63a83a6
Calil Brandão <cbrandao2001@gmail.com>
2024-04-05 15:49:53 -0300

    date clear

--- src/Date.java
+++ src/Date.java
@@ -1,3 +1,16 @@
 public class Date {
-    
+    private int day;
+    private String month;
+    private int year;
+
+
+    public Date(int d, String m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+
+    public void dReport() {
+        System.out.println(day + " " + month + " " + year);
+    }
 }
