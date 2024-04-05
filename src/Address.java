b7b707da0ca21aa385b45fa044bd09ecd963f344
Calil Brandão <cbrandao2001@gmail.com>
2024-04-05 15:51:12 -0300

    patient clear

--- src/Address.java
+++ src/Address.java
@@ -1,3 +1,16 @@
 public class Address {
-    
+    private String street;
+    private int cep;
+    private int number;
+
+    public Address(String s, int cep, int n) {
+        street = s;
+        this.cep = cep;
+        number = n;
+    }
+
+    public void aReport(){
+        System.out.println("Street " + street + ", Number " + number + ", CEP " + cep);
+    }
+
 }
