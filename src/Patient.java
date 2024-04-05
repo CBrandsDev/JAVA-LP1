f22185a3b51cf16dc54fc4832719450498b9a8ab
Calil Brandão <cbrandao2001@gmail.com>
2024-04-05 15:50:45 -0300

    patient clear

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,22 @@
 public class Patient {
+    private String name;
+    private int age;
+    private Address local;
+    private Exam typeExam;
     
+    public Patient(String n, int a, Address l, Exam t) {
+        name = n;
+        age = a;
+        local = l;
+        typeExam = t;
+    }
+
+    public void pReport() {
+        System.out.println("Patient Name: " + name);
+        System.out.println("Patient Age: " + age);
+        System.out.println("Patient Address: ");
+        local.aReport();
+        System.out.println("Patient Exam: ");
+        typeExam.eReport();
+    }
 }
