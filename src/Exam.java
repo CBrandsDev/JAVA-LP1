e84ec1b9b82c8b6209b4407840c6be964cd3b92f
Calil Brandão <cbrandao2001@gmail.com>
2024-04-05 15:50:20 -0300

    exam clear

--- src/Exam.java
+++ src/Exam.java
@@ -1,3 +1,24 @@
 public class Exam {
-    
+    private String description;
+    private int code;
+    private double value;
+    private Date examDate;
+
+    public Exam(String ds, int code, double v, Date dt) {
+        description = ds;
+        this.code = code;
+        value = v;
+        examDate = dt;
+    }
+
+    public void eReport() {
+        System.out.println("Exam: " + description);
+        System.out.println("Exam Code: " + code);
+        System.out.println("Date: ");
+        examDate.dReport();
+        System.out.println("Exam Value: " + value);
+
+    }
+
+
 }
